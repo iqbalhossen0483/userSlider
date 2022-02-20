e4bd808c7baa697da2a9099b957d08987306bc10
Md Rakib <rayhan.senbag.bd@gmail.com>
2022-02-20 21:48:23 +0600

    lskdagjio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import usersData from './data/user';
 
 function App() {
+  const [numberOfSlider, setNumberOfSlider] = useState(0);
+  const users = usersData;
+
+
+  function increaseSliderNumber() {
+    if (numberOfSlider === users.length -1) return;
+    setNumberOfSlider(numberOfSlider + 1);
+  };
+
+  function decreaseSliderNumber() {
+    if (numberOfSlider === 0) return;
+    setNumberOfSlider(numberOfSlider - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      <div className="container">
+          {/* slider section */}
+        <div className="slider-container">
+          <p className="arrow">
+            <button onClick={decreaseSliderNumber}>&#8592;</button>
+            <button onClick={increaseSliderNumber}>&#8594;</button>
+          </p>
+          {
+            users.map((user, index) => <div
+              key={user.name}
+              className={`slider ${numberOfSlider === index && "show"}`}>
+              <p>{user.name}</p>
+              <p>Class: {user.class}</p>
+              <p>No: {user.slider}</p>
+              <p>All details will goes here</p>
+            </div>)
+          }
+        </div>
+
+        {/* user section */}
+        <div className='user-container'>
+          {
+            users.map((user, index) => <p
+              key={user.name}
+              className={`user ${numberOfSlider === index && "highlight"}`}>
+              {user.name.charAt(0)}
+            </p>)
+          }
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data/user.js
@@ -0,0 +1,24 @@
+const usersData = [
+    {
+        name: "Rakib",
+        class: 5,
+        slider: 1
+    },
+    {
+        name: "Raihan",
+        class: 6,
+        slider: 2
+    },
+    {
+        name: "Tania",
+        class: 7,
+        slider: 3
+    },
+    {
+        name: "Taspia",
+        class: 8,
+        slider: 4
+    },
+];
+
+export default usersData;
